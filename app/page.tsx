87315872eba97382e1ba6b1235ef3fc2b70aaf7d
iamyelex <yelex4love@gmail.com>
2023-10-29 23:49:06 -0700

    user location detected

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { UserLocationContext } from "@/context/UserLocationContext";
 import Booking from "@/app/(booking)/Booking";
 import MapBox from "@/app/(map)/MapBox";
 
+type userLocationProps = {
+  lat: number;
+  lng: number;
+};
+
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<userLocationProps>();
+
+  const getUserLocation = function () {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setUserLocation({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+    });
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
   return (
-    <section className="grid grid-cols-1 md:grid-cols-3">
-      <div>
-        <Booking />
-      </div>
-      <div className="col-span-2">
-        <MapBox />
-      </div>
-    </section>
+    <UserLocationContext.Provider value={(userLocation, setUserLocation)}>
+      <section className="grid grid-cols-1 md:grid-cols-3">
+        <div>
+          <Booking />
+        </div>
+        <div className="col-span-2">
+          <MapBox />
+        </div>
+      </section>
+    </UserLocationContext.Provider>
   );
 }
